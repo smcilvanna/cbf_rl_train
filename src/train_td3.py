f7242f9a64c1e7478306aa9bbdcae56a44ea3220
smcilvanna <smcilvanna01@qub.ac.uk>
2024-10-16 10:20:03 +0100

    update td3 train script with adjusted hps for better training performance

--- src/train_td3.py
+++ src/train_td3.py
@@ -6,7 +6,7 @@ from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.callbacks import CheckpointCallback
 # from itertools import product
 # import time
-# import numpy as np
+import numpy as np
 import os
 import rospy
 
@@ -26,53 +26,48 @@ class CustomCheckpointCallback(CheckpointCallback):
                 print(f"Saving model and replay buffer to {path}")
         return True
 
-def train_td3(learning_rate, gamma, batch_size, train_steps=100):    
-    env = gym.make('cbf-train-gzros-td3')          # Create the environment
-    model = TD3(                                   # Define the TD3 model
-        "MlpPolicy",                                # Policy type (multi-layer perceptron)
-        env,                                        # the custom environment
-        verbose=                1,                  # Verbosity mode
-        learning_rate=          learning_rate,      # Learning rate passed as an argument
-        gamma=                  gamma,              # Discount factor passed as an argument
-        batch_size=             batch_size,         # Batch size for training
-        buffer_size=            1000,               # Replay buffer size
-        learning_starts=        1,                  # Number of steps before training starts
-        policy_delay=           1,                  # Policy update delay 1=update immediately (no delay in reward in this scenario)
-        action_noise=           None,               # Action noise (minimal noise)
-        target_policy_noise=    0.0,
-        policy_kwargs=          dict(net_arch=[64, 64]),    # Policy network architecture
-        tensorboard_log="/home/user/husky/tensorboard/td3", # Path to the directory where TensorBoard logs will be saved, uncomment for logging
+def train_td3(learning_rate= 1e-3, gamma= 0.00, batch_size= 50, train_steps=100):    
+    env = gym.make('cbf-train-gzros-td3')                               # Create the environment
+    n_acions = env.action_space.shape[-1]                               # Number of actions (for action noise parameter)
+    tb_path = "/home/user/husky/tensorboard/td3"                        # Path to the directory where TensorBoard logs will be saved
+    action_noise = NormalActionNoise(mean=np.zeros(n_acions),
+                                      sigma=0.1 * np.ones(n_acions))    # Action noise to explore the environment, increase sigma for more noise/exploration
+    model = TD3(                                          # Define the TD3 model
+        "MlpPolicy",                                        # Policy type (multi-layer perceptron)
+        env,                                                # the custom environment
+        learning_rate=          learning_rate,              # Learning rate passed as an argument
+        gamma=                  gamma,                      # Discount factor passed as an argument
+        batch_size=             batch_size,                 # Batch size for training
+        action_noise=           action_noise,               # Action noise (minimal noise)
+        policy_kwargs=          dict(net_arch=[64, 64]),    # Policy network architecture ######## COULD CONSIDER ADDING : learning_rate_actor=1e-3, learning_rate_critic=1e-4 for separate learning rates 
+        tensorboard_log=        tb_path,                    # Tensorboard path
+        verbose=                1,                          # Verbosity mode
+        buffer_size=            5000,                       # Replay buffer size
+        learning_starts=        1,                          # Number of steps before training starts
+        policy_delay=           2,                          # Policy update delay 2 : actor (policy) network is updated once for every two updates of the critic (Q) network, allows the critic to better estimate the value function before updating the policy
+        target_policy_noise=    0.2,                        # Adds noise to target actions which promotes exploration during updates, helping avoid local optima
         tau=                    0.05                        # Target network update coefficient, slightly larger for deterministic environment
     )
 
-    checkpoint_callback = CustomCheckpointCallback(       # Callback to save the model and replay buffer every 10 steps/episodes
+    checkpoint_callback = CustomCheckpointCallback(       # Custom callback to save the model and replay buffer during training in case of crash
         save_freq=100, 
         save_path='/home/user/husky/td3_models/',
-        name_prefix=f"td3_run{run_id}_chkpt"
-    )
+        name_prefix=f"td3_run{run_id}_chkpt")
 
     # Train the model
     model.learn(total_timesteps=train_steps,
                 callback=checkpoint_callback,
-                log_interval=1)                    # train the model
+                log_interval=1)                             # Train TD3 model
     
+    fin_runs = model._n_updates                                                                                 # Number of training runs for file tag
     model.save(f"/home/user/husky/td3_models/final/td3_run{run_id}_model_{fin_runs}")                           # Save the model
     model.save_replay_buffer(f"/home/user/husky/td3_models/final/td3_run{run_id}_replay_buffer_{fin_runs}")     # Save the replay buffer
 
     return model
 
 if __name__ == "__main__":
-    
-    run_id = 4
-    fin_runs = 2000
+    run_id = 5                                      # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Run ID for file tag - CHANGE EVERY TRAINING SESSION !!!!!!!!!
     rospy.init_node('gymnode', anonymous=True)      # Initialize the node
-
-    learnrate = 1e-3
-    gamma     = 0.00
-    batch     = 1
-    steps = 2000
-    model = train_td3(learning_rate=learnrate, 
-                        gamma=gamma, 
-                        batch_size=batch,
-                        train_steps=steps)
-
+    steps = 2000                                    # Number of training steps
+    model = train_td3(train_steps=steps)            # Train the TD3 model
+    print(">>>>>>>>>>>>>===============<<<<<<<<<<<<<<\n           TRAINING COMPLETE!             \n>>>>>>>>>>>>>===============<<<<<<<<<<<<<<")
