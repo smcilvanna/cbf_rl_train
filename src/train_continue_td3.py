5a382808063cb090008bfb2a6e5d0a61ea70d545
smcilvanna <smcilvanna01@qub.ac.uk>
2024-10-16 14:27:22 +0100

    updated continue script to match train for td3

--- src/train_continue_td3.py
+++ src/train_continue_td3.py
@@ -20,66 +20,37 @@ class CustomCheckpointCallback(CheckpointCallback):
             # Save the model
             path = os.path.join(self.save_path, f'{self.name_prefix}_{self.n_calls}_steps')
             self.model.save(path)
-
             # Save the replay buffer
             replay_buffer_path = os.path.join(self.save_path, f'{self.name_prefix}_{self.n_calls}_replay_buffer')
             self.model.save_replay_buffer(replay_buffer_path)
-
             if self.verbose > 1:
                 print(f"Saving model and replay buffer to {path}")
-
         return True
 
-def train_td3(learning_rate, gamma, batch_size, train_steps=100):    
-    env = gym.make('cbf-train-gzros')          # Create the environment
-    # n_actions = env.action_space.shape[-1]
-    # action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.01 * np.ones(n_actions))  # Add minimum noise to the action space
-    # model = TD3(                        # Define the TD3 model
-    #     "MlpPolicy",                                # Policy type (multi-layer perceptron)
-    #     env,                                        # the custom environment
-    #     verbose=                1,                  # Verbosity mode
-    #     learning_rate=          learning_rate,      # Learning rate passed as an argument
-    #     gamma=                  gamma,              # Discount factor passed as an argument
-    #     batch_size=             batch_size,         # Batch size for training
-    #     buffer_size=            1000,               # Replay buffer size
-    #     learning_starts=        1,                  # Number of steps before training starts
-    #     policy_delay=           1,                  # Policy update delay 1=update immediately (no delay in reward in this scenario)
-    #     action_noise=           action_noise,       # Action noise (minimal noise)
-    #     target_policy_noise=    0.0,
-    #     policy_kwargs=          dict(net_arch=[64, 64]),  # Policy network architecture
-    #     #tensorboard_log="./td3_tensorboard/"       # Path to the directory where TensorBoard logs will be saved, uncomment for logging
-    #     tau=            0.05                        # Target network update coefficient, slightly larger for deterministic environment
-    # )
-    model = TD3.load("/home/user/husky/td3_models/final/td3_model_run2_13k", env=env)  # Load the model from the saved file
+def train_td3(train_steps):    
+    env = gym.make('cbf-train-gzros-td3')                                               # Create the environment
+    model =         TD3.load("/home/user/husky/td3_models/final/td3_run5_model_1999", env=env)   # Load the model from the saved file
+    model.load_replay_buffer("/home/user/husky/td3_models/final/td3_run5_replay_buffer_1999")    # Load the replay buffer
     
-    checkpoint_callback = CustomCheckpointCallback(       # Callback to save the model and replay buffer
+    checkpoint_callback = CustomCheckpointCallback(       # Custom callback to save the model and replay buffer during training in case of crash
         save_freq=100, 
         save_path='/home/user/husky/td3_models/',
-        name_prefix="td3_chkpt"
-    )
+        name_prefix=f"td3_run{run_id}_chkpt")
 
-    model.load_replay_buffer("/home/user/husky/td3_models/final/td3_replay_buffer_run2_13k")  # Load the replay buffer
-    # Train the model
-    #model.learn(total_timesteps=train_steps, log_interval=10)  # with tensorboard logging
     model.learn(total_timesteps=train_steps,
-                callback=checkpoint_callback)                    # without tensorboard logging, with checkpoint callback
+                callback=checkpoint_callback,
+                log_interval=1)                    
     
-    model.save("/home/user/husky/td3_models/final/td3_model_run2_15k")          # Save the model
-    model.save_replay_buffer("/home/user/husky/td3_models/final/td3_replay_buffer_run2_15k")  # Save the replay buffer
-    
+    fin_runs = model._n_updates                                                                                 # Number of training runs for file tag
+    model.save(              f"/home/user/husky/td3_models/final/td3_run{run_id}_model_{fin_runs}")             # Save the model
+    model.save_replay_buffer(f"/home/user/husky/td3_models/final/td3_run{run_id}_replay_buffer_{fin_runs}")     # Save the replay buffer
+
     return model
 
 if __name__ == "__main__":
-    
+    run_id = 5                                      # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Run ID for file tag - CHANGE EVERY TRAINING SESSION !!!!!!!!!
     rospy.init_node('gymnode', anonymous=True)      # Initialize the node
-
-    learnrate = 1e-3
-    gamma     = 0.00
-    batch     = 10
-
     steps = 2000
-    model = train_td3(learning_rate=learnrate, 
-                        gamma=gamma, 
-                        batch_size=batch,
-                        train_steps=steps)
+    model = train_td3(train_steps=steps)
+    print(">>>>>>>>>>>>>===============<<<<<<<<<<<<<<\n           TRAINING COMPLETE!             \n>>>>>>>>>>>>>===============<<<<<<<<<<<<<<")
 
